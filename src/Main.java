813b052ec8489fa9ab0faa0f4a2865c758f04ee6
datron <kartik1702@gmail.com>
2018-01-24 20:52:48 +0530

    Added audio Player that works

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import sun.audio.AudioPlayer;
+
+import javax.sound.sampled.AudioInputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main extends Application {
+    public static void main(String[] args) throws IOException, InterruptedException {
         URL url = null;
         try {
-            url = new URL("https://rss.art19.com/masters-of-scale");
+            url = new URL("https://www.npr.org/rss/podcast.php?id=510313");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -17,14 +24,23 @@ public class Main {
         System.out.println("Description:"+pod.getPodDescription());
         System.out.println("Image:"+pod.getImage());
         System.out.println();
-        for (int i=0;i < pod.numberOfEpisodes ; i++){
-            System.out.println("Episode Title:"+pod.episodes.get(i).getTitle());
-            System.out.println("Episode Description:"+pod.episodes.get(i).getDescription());
-            System.out.println("Episode Date:"+pod.episodes.get(i).getDate());
-            System.out.println("Episode Duration:"+pod.episodes.get(i).getDuration());
-            System.out.println("Episode Link:"+pod.episodes.get(i).getMp3link());
+        for (int i=0;i < pod.numberOfEpisodes ; i++) {
+            System.out.println("Episode Title:" + pod.episodes.get(i).getTitle());
+            System.out.println("Episode Description:" + pod.episodes.get(i).getDescription());
+            System.out.println("Episode Date:" + pod.episodes.get(i).getDate());
+            System.out.println("Episode Duration:" + pod.episodes.get(i).getDuration());
+            System.out.println("Episode Link:" + pod.episodes.get(i).getMp3link());
             System.out.println();
         }
+        Player player = new Player(pod.episodes.get(0).getMp3link());
+        Application.launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox vBox = new VBox();
+        Scene s = new Scene(vBox,500,400);
+        primaryStage.setScene(s);
+        primaryStage.show();
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,46 @@
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.util.regex.Pattern;
+
+
 public class Player {
+    private Media media;
+    private MediaPlayer mediaPlayer;
+    private long position;
+    private boolean isPaused;
+
+
+    Player(String mp3url){
+        System.out.println(mp3url);
+        media = new Media(mp3url);
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setOnReady(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Duration:"+media.getDuration().toMinutes());
+                mediaPlayer.play();
+            }
+        });
+    }
+
+    void play(){
+        mediaPlayer.play();
+    }
+    void forward(){
+
+    }
+    void backward(){
+
+    }
+    void pause(){
+
+    }
+    void resume(){
+
+    }
+    void goToPosition(){
+
+    }
 }
